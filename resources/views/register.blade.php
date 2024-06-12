ba2fd47f8eebea7a709e18caab50f1534d9e54e1
kito <surya.aw26@gmail.com>
2024-06-12 19:02:32 +0700

    add card for preview image, validation for image size and show input error messages in register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -36,59 +36,106 @@
 
           <div class="form-row">
             <div class="form-group col-md-4">
-              <label for="inputEmail4">Nama Prodi</label>
-              <select class="form-control" name="" id="">
-                <option value="" selected disabled>Pilih prodi</option>
+              <label for="prodi">Nama Prodi</label>
+              <select class="form-control" name="prodi" id="">
+                <option value="NULL" selected disabled>Pilih prodi</option>
+                <option value="sistem_informasi" {{ old('prodi') == 'sistem_informasi' ? 'selected' : '' }}>Sistem Informasi</option>
+                <option value="teknik_informatika" {{ old('prodi') == 'teknik_informatika' ? 'selected' : '' }}>Teknik Informatika</option>
               </select>
+
+              @error('prodi')
+              <div class="text-danger">{{ $message }}</div>
+              @enderror
             </div>
             <div class="form-group col-md-4">
               <label for="namaMahasiswa">Nama Mahasiswa</label>
-              <input type="text" name="namaMhs" class="form-control" id="namaMahasiswa" placeholder="Nama Mahasiswa">
+              <input type="text" name="namaMhs" class="form-control" id="namaMahasiswa" placeholder="Nama Mahasiswa" value="{{ old('namaMhs') }}">
+
+              @error('namaMhs')
+              <div class="text-danger">{{ $message }}</div>
+              @enderror
             </div>
             <div class="form-group col-md-4">
               <label for="nim">NIM</label>
-              <input type="text" name="nim" class="form-control" id="nim" placeholder="NIM">
+              <input type="text" name="nim" class="form-control" id="nim" placeholder="NIM" value="{{ old('nim') }}">
+
+              @error('nim')
+              <div class="text-danger">{{ $message }}</div>
+              @enderror
             </div>
           </div>
 
           <div class="form-row">
             <div class="form-group col-md-6">
               <label for="noKTP">No. KTP</label>
-              <input type="text" name="nik" class="form-control" id="noKTP" placeholder="No. KTP">
+              <input type="text" name="nik" class="form-control" id="noKTP" placeholder="No. KTP" value="{{ old('nik') }}">
+
+              @error('nik')
+              <div class="text-danger">{{ $message }}</div>
+              @enderror
             </div>
             <div class="form-group col-md-6">
               <label for="namaIbuKandung">Nama Ibu Kandung</label>
-              <input type="text" class="form-control" id="namaIbuKandung" placeholder="Nama Ibu Kandung">
+              <input type="text" name="namaIbuKandung" class="form-control" id="namaIbuKandung"
+                placeholder="Nama Ibu Kandung" value="{{ old('namaIbuKandung') }}">
+
+              @error('namaIbuKandung')
+              <div class="text-danger">{{ $message }}</div>
+              @enderror
             </div>
           </div>
 
           <div class="form-row">
             <div class="form-group col-md-4">
               <label for="tempatLahir">Tempat Lahir</label>
-              <input type="text" class="form-control" id="tempatLahir" placeholder="Tempat Lahir">
+              <input type="text" name="tempatLahir" class="form-control" id="tempatLahir" placeholder="Tempat Lahir" value="{{ old('tempatLahir') }}">
+
+              @error('tempatLahir')
+              <div class="text-danger">{{ $message }}</div>
+              @enderror
             </div>
             <div class="form-group col-md-4">
               <label for="tglLahir">Tanggal Lahir</label>
-              <input type="date" class="form-control" id="tglLahir">
+              <input type="date" name="tglLahir" class="form-control" id="tglLahir"  value="{{ old('tglLahir') }}">
+
+              @error('tglLahir')
+              <div class="text-danger">{{ $message }}</div>
+              @enderror
             </div>
             <div class="form-group col-md-4">
               <label for="tglMasuk">Tanggal Masuk</label>
-              <input type="date" class="form-control" id="tglMasuk">
+              <input type="date" name="tglMasuk" class="form-control" id="tglMasuk"  value="{{ old('tglMasuk') }}">
+
+              @error('tglMasuk')
+              <div class="text-danger">{{ $message }}</div>
+              @enderror
             </div>
           </div>
 
           <div class="form-row">
             <div class="form-group col-md-4">
               <label for="gelarMahasiswa">Gelar Mahasiswa</label>
-              <input type="text" class="form-control" id="gelarMahasiswa" placeholder="Gelar Mahasiswa">
+              <input type="text" name="gelarMahasiswa" class="form-control" id="gelarMahasiswa" placeholder="Gelar Mahasiswa" value="{{ old('gelarMahasiswa') }}">
+
+              @error('gelarMahasiswa')
+              <div class="text-danger">{{ $message }}</div>
+              @enderror
             </div>
             <div class="form-group col-md-4">
               <label for="email">Email</label>
-              <input type="email" class="form-control" id="email" placeholder="Email">
+              <input type="email" name="email" class="form-control" id="email" placeholder="Email" value="{{ old('email') }}">
+
+              @error('email')
+              <div class="text-danger">{{ $message }}</div>
+              @enderror
             </div>
             <div class="form-group col-md-4">
               <label for="password">Password</label>
-              <input type="password" class="form-control" id="password" placeholder="Password">
+              <input type="password" name="password" class="form-control" id="password" placeholder="Password" value="{{ old('password') }}">
+
+              @error('password')
+              <div class="text-danger">{{ $message }}</div>
+              @enderror
             </div>
           </div>
 
@@ -96,17 +143,21 @@
             <div class="form-group col-md-4">
               <label for="fotoMahasiswa">Foto Mahasiswa</label>
               <input type="file" name="fotoMahasiswa" class="form-control" id="fotoMahasiswa" accept="image/*">
-              <img src="#" id="previewFotoMahasiswa" class="d-none">
+              <br>
+              <div class="card d-none" id="cardPreviewFotoMahasiswa" style="max-width: 300px; max-height: 300px">
+                <img src="#" id="previewFotoMahasiswa" style="object-fit: cover">
+              </div>
             </div>
           </div>
           <br>
           <button type="submit" class="btn btn-success">Submit</button>
-          <a href="{{ url()->previous() }}" role="button" class="btn btn-info">Kembali</a>
+          <a href="{{ route('login') }}" role="button" class="btn btn-info">Kembali</a>
         </form>
       </div>
     </div>
   </div>
 
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
     crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
@@ -126,7 +177,19 @@
     }
 
     $("#fotoMahasiswa").change(function() {
-      $('#previewFotoMahasiswa').removeClass('d-none')
+      if (this.files[0].size > 3145728) { // if image size greater than 3mb
+        $(this).val(null)
+        $('#cardPreviewFotoMahasiswa').remove()
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Ukuran foto yang dipilih terlalu besar!",
+        });
+
+        return
+      }
+
+      $('#cardPreviewFotoMahasiswa').removeClass('d-none')
       previewImage(this)
     });
   </script>
