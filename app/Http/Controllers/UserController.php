be1beebb8930f958bc2949adee520024d32ce157
kito <surya.aw26@gmail.com>
2024-06-12 19:03:57 +0700

    add method for login and use sweet alert if there is error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
@@ -16,10 +18,44 @@ class UserController extends Controller
     {
         $usn = $request->username;
         $password = $request->password;
+
+        $user = DB::table('user')->where('USERNAME', $usn)->first();
+
+        if (!$user || !Hash::check($password, $user->PASSWORD)) {
+            return redirect()->back()->with('message', 'Username atau password salah!');
+        }
+
+        $encryptedUserID = encrypt($user->ID);
+        Session::put('isLogin', $encryptedUserID);
+
+        return redirect()->route('home');
     }
 
     public function registerView()
     {
         return view('register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate(
+            [
+                'prodi' => 'required',
+                'namaMhs' => 'required',
+                'nim' => 'required',
+                'nik' => 'required',
+                'namaIbuKandung' => 'required',
+                'email' => 'required|email',
+                'password' => 'required',
+                'fotoMahasiswa' => 'file|size:4000',
+            ],
+            [
+                'prodi.required' => 'Prodi belum diisi!',
+                'namaMhs.required' => 'Nama mahasiswa belum diisi!',
+                'nim.required' => 'NIM mahasiswa belum diisi!',
+                'nik.required' => 'Nomor KTP belum diisi!',
+                'namaIbuKandung.required' => 'Nama ibu kandung belum diisi!',
+            ]
+        );
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AdminLTE 3 | Log in</title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
   <!-- icheck bootstrap -->
@@ -14,57 +16,68 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition login-page">
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
-    {{-- <div class="card-header text-center">
-      <a href="../../index2.html" class="h1"><b>Admin</b>LTE</a>
-    </div> --}}
-    <div class="card-body">
-      <p class="login-box-msg">Silahkan Login</p>
+  <div class="login-box">
+    <div class="card card-outline card-primary">
+      <div class="card-body">
+        <p class="login-box-msg">Silahkan Login</p>
 
-      <form action="{{ route('action-login') }}" method="post">
-        @csrf
-        <div class="input-group mb-3">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
+        <form action="{{ route('action-login') }}" method="post">
+          @csrf
+
+          <div class="input-group mb-3">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
             </div>
+            <input type="text" class="form-control" name="username" placeholder="Username" required
+              value="{{ old('username') }}">
           </div>
-          <input type="text" class="form-control" name="username" placeholder="Username" required>
-        </div>
-        <div class="input-group mb-3">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+          <div class="input-group mb-3">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
             </div>
+            <input type="password" class="form-control" name="password" placeholder="Password" required>
           </div>
-          <input type="password" class="form-control" name="password" placeholder="Password" required>
-        </div>
-        <div class="row">
-          <div class="col-6">
-            <button type="submit" class="btn btn-primary btn-block">Masuk</button>
+          <div class="row">
+            <div class="col-6">
+              <button type="submit" class="btn btn-primary btn-block">Masuk</button>
+            </div>
+            <!-- /.col -->
+            <div class="col-6">
+              <a href="{{ route('register') }}" role="button" class="btn btn-primary btn-block">Registrasi</a>
+            </div>
+            <!-- /.col -->
           </div>
-          <!-- /.col -->
-          <div class="col-6">
-            <a href="{{ route('register') }}" role="button" class="btn btn-primary btn-block">Registrasi</a>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
+        </form>
+      </div>
+      <!-- /.card-body -->
     </div>
-    <!-- /.card-body -->
   </div>
-  <!-- /.card -->
-</div>
-<!-- /.login-box -->
+  <!-- /.login-box -->
 
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+  <!-- jQuery -->
+  <script src="../../plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../../dist/js/adminlte.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script>
+    let message = "{{ session('message') }}"
+
+    if (message && message != ' ') {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: message,
+      });
+    }
+  </script>
 </body>
+
 </html>
