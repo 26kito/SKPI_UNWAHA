c6ed5a2cf0853c2fec89f79ca33316019ce15d2e
kito <surya.aw26@gmail.com>
2024-06-14 21:23:23 +0700

    change content in home based on user role

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class HomeController extends Controller
+{
+    public function home()
+    {
+        $data = null;
+
+        if (User::authUser()->ROLE == 'ADMIN') {
+            $data = DB::table('skpi')
+                ->selectRaw("
+                COUNT(*) AS total_mengajukan,
+                COUNT(DISTINCT USER_ID) AS total_mhs,  
+                (SELECT COUNT(*) FROM skpi WHERE STATUS_ID = 1) AS total_menunggu,
+                (SELECT COUNT(DISTINCT USER_ID) FROM skpi WHERE STATUS_ID = 1) AS total_mhs_menunggu,
+                (SELECT COUNT(*) FROM skpi WHERE STATUS_ID = 2) AS total_disetujui,
+                (SELECT COUNT(DISTINCT USER_ID) FROM skpi WHERE STATUS_ID = 2) AS total_mhs_disetujui,
+                (SELECT COUNT(*) FROM skpi WHERE STATUS_ID = 3) AS total_ditolak,
+                (SELECT COUNT(DISTINCT USER_ID) FROM skpi WHERE STATUS_ID = 3) AS total_mhs_ditolak")
+                ->first();
+        }
+
+        return view('index')->with('data', $data);
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,13 +16,11 @@
 
 @section('content')
 <div class="card">
+    @if (Helper::authUser()->ROLE == 'ADMIN')
     <div class="card-body">
         <h3>Selamat Datang</h3>
         <br>
         <h5 style="text-align: center;">Informasi Portofolio</h5>
-        <section class="content">
-
-        </section>
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 col-sm-6 col-md-3">
@@ -31,7 +29,7 @@
                         <div class="info-box-content">
                             <span class="info-box-content">TOTAL YANG SUDAH MENGAJUKAN</span>
                             <span class="info-box-number">
-                                5
+                                {{ $data->total_mengajukan }}
                             </span>
                         </div>
                     </div>
@@ -42,7 +40,7 @@
                         <div class="info-box-content">
                             <span class="info-box-content">MENUNGGU PERSETUJUAN</span>
                             <span class="info-box-number">
-                                2
+                                {{ $data->total_menunggu }}
                             </span>
                         </div>
                     </div>
@@ -53,7 +51,7 @@
                         <div class="info-box-content">
                             <span class="info-box-content">PENGAJUAN YANG DITOLAK</span>
                             <span class="info-box-number">
-                                0
+                                {{ $data->total_ditolak }}
                             </span>
                         </div>
                     </div>
@@ -64,7 +62,7 @@
                         <div class="info-box-content">
                             <span class="info-box-content">PENGAJUAN YANG DISETUJUI</span>
                             <span class="info-box-number">
-                                3
+                                {{ $data->total_disetujui }}
                             </span>
                         </div>
                     </div>
@@ -77,7 +75,7 @@
                         <div class="info-box-content">
                             <span class="info-box-content">TOTAL MAHASISWA</span>
                             <span class="info-box-number">
-                                2
+                                {{ $data->total_mhs }}
                             </span>
                         </div>
                     </div>
@@ -88,7 +86,7 @@
                         <div class="info-box-content">
                             <span class="info-box-content">JUMLAH MAHASISWA</span>
                             <span class="info-box-number">
-                                2
+                                {{ $data->total_mhs_menunggu }}
                             </span>
                         </div>
                     </div>
@@ -99,7 +97,7 @@
                         <div class="info-box-content">
                             <span class="info-box-content">JUMLAH MAHASISWA</span>
                             <span class="info-box-number">
-                                0
+                                {{ $data->total_mhs_ditolak }}
                             </span>
                         </div>
                     </div>
@@ -110,14 +108,33 @@
                         <div class="info-box-content">
                             <span class="info-box-content">JUMLAH MAHASISWA</span>
                             <span class="info-box-number">
-                                0
+                                {{ $data->total_mhs_disetujui }}
                             </span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <table id="myTable" class="display">
+
+        <table id="homeTable" class="display">
+            <thead>
+                <tr>
+                    <th>Nama Mahasiswa</th>
+                    <th>Kategori Portofolio</th>
+                    <th>Nama Portofolio</th>
+                    <th>Kesesuaian Portofolio</th>
+                    <th>Tanggal Portofolio</th>
+                    <th>No. Dokumen Portofolio</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody id="homeTableContent">
+            </tbody>
+        </table>
+    </div>
+    @else
+    <div class="card-body">
+        <table id="homeTable" class="display">
             <thead>
                 <tr>
                     <th class="text-center" style="width: 30%">Tanggal</th>
@@ -138,6 +155,7 @@
             </tbody>
         </table>
     </div>
+    @endif
 </div>
 @endsection
 
@@ -148,6 +166,36 @@
 <script src="https://cdn.datatables.net/2.0.8/js/dataTables.bootstrap4.js"></script>
 <script>
     $(document).ready( function () {
-        new DataTable('#myTable');
+        new DataTable('#homeTable');
+
+        let userRole = "{{ Helper::authUser()->ROLE }}"
+
+        if (userRole == 'ADMIN') {
+            $.ajax({
+                type: 'GET',
+                url: '/data/all/skpi',
+                success: (result) => {
+                    let table = ''
+    
+                    result.forEach((d) => {
+                        table += `
+                            <tr>
+                                <td>${d.NAMA_MAHASISWA}</td>
+                                <td>${d.KATEGORI_PORTOFOLIO}</td>
+                                <td>${d.NAMA_PORTOFOLIO}</td>
+                                <td>${d.IS_SESUAI}</td>
+                                <td>${d.TANGGAL_PORTOFOLIO}</td>
+                                <td>${d.NO_DOKUMEN}</td>
+                                <td class="fw-bolder ${d.status_text}">${d.STATUS}</td>
+                            </tr>
+                        `
+    
+                        return table
+                    })
+    
+                    $('#homeTableContent').html(table)
+                }
+            })
+        }
     })
 </script>
