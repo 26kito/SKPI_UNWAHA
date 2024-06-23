72f3f8d36c293554d76821a554724103c72bfc76
kito <surya.aw26@gmail.com>
2024-06-23 21:32:54 +0700

    modify view validated skpi

--- resources/views/validate-skpi.blade.php
+++ resources/views/validate-skpi.blade.php
@@ -20,20 +20,48 @@
         <div class="text-center">
             <img src="https://unwaha.ac.id/wp-content/uploads/2022/12/logo_besar.png" alt="">
             <h3 class="text-danger mt-4">UNIVERSITAS KH. A. WAHAB HASBULLAH</h3>
-            <h3>DRAFT SURAT KETERANGAN PENDAMPING IJAZAH (SKPI)</h3>
+            <h3>SURAT KETERANGAN PENDAMPING IJAZAH (SKPI)</h3>
             <p>Nomor: {{ $data->NO_SKPI }}</p>
         </div>
-        <table class="table">
+        <table class="table table-bordered">
             <tr>
-                <th>apalah</th>
+                <th>Informasi Mahasiswa</th>
             </tr>
             <tr>
-                <td>konten</td>
-                <td>konten</td>
+                <td style="width: 40%">Nama</td>
+                <td>{{ $data->NAMA }}</td>
             </tr>
             <tr>
-                <td>isi konten lainnya...</td>
-                <td>isi konten lainnya...</td>
+                <td>NIM</td>
+                <td>{{ $data->NIM }}</td>
+            </tr>
+            <tr>
+                <td>Program Studi</td>
+                <td>{{ $data->PRODI }}</td>
+            </tr>
+            <tr>
+                <td>Tempat & Tanggal Lahir</td>
+                <td>{{ "$data->TEMPAT_LAHIR, $data->TANGGAL_LAHIR" }}</td>
+            </tr>
+            <tr>
+                <td>Tanggal Masuk</td>
+                <td>{{ $data->TANGGAL_MASUK }}</td>
+            </tr>
+            <tr>
+                <td>Tanggal Lulus</td>
+                <td>{{ $data->TANGGAL_LULUS }}</td>
+            </tr>
+            <tr>
+                <td>Lama Studi</td>
+                <td>{{ $data->LAMA_STUDI }}</td>
+            </tr>
+            <tr>
+                <td>Gelar</td>
+                <td>{{ "Sarjana Komputer ($data->GELAR)" }}</td>
+            </tr>
+            <tr>
+                <td>Bahasa Pengantar Kuliah</td>
+                <td>Bahasa Indonesia</td>
             </tr>
         </table>
     </div>
